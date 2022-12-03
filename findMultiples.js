415a2f493f0405451e23d73b288c228ad2dd0e1f
lotfy669 <M.lotfy9966@gmail.com>
2022-12-03 15:14:26 +0200

    find Multiples

---
+++ findMultiples.js
@@ -0,0 +1,10 @@
+// findMultiples
+function findMultiples(integer, limit) {
+  let arr = [];
+  for (let i = integer; i <= limit; i += integer) {
+    arr.push(i);
+  }
+  return arr;
+}
+
+console.log(findMultiples(5, 25));
